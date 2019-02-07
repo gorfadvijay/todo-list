9a305b82c2160ee7806d555fa04c22e83e294a14
Vijay Gorfad <gorfadvijay1234@gmail.com>
2019-02-07 17:51:29 +0530

    Added Components With Todos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Todos from './components/Todos'
+
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos :[
+      {
+        id: 1,
+        title: 'hello',
+        completed:false
+
+      },
+      {
+        id: 2,
+        title: 'Learn Redux',
+        completed:false
+
+      },
+      {
+        id: 3,
+        title: 'Take A rest',
+        completed:false
+
+      }
+    ]
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hello World</h1>
       </div>
     );
   }
